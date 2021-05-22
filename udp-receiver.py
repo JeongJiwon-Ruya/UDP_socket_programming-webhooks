046deb261fbb94d8617ed9c0419af4b6f7ee9c7a
JeongJiwon <freud.ioru@gmail.com>
2021-05-23 05:02:17 +0900

    update 0523

---
+++ udp-receiver.py
@@ -0,0 +1,16 @@
+import socket
+import time
+
+UDP_IP = "0.0.0.0"
+UDP_PORT = 5001
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind((UDP_IP, UDP_PORT))
+
+print("UDP Server started !")
+while True:
+    data, addr = socket.recvfrom(1024) # buffer size is 1024 bytes
+    print("Updated :::: received message: ", data.decode('utf8'), " from " , addr)
+    currentTime = " " + time.ctime(time.time()) + "\r\n"
+    data = data + currentTime.encode('ascii')
+    sock.sendto(data,addr)
